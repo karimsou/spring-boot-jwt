f229bf6135b590b519f68c563b2f169ab616184a
Karim Souabni <karim.souabni1@gmail.com>
2024-06-10 10:46:00 -0300

    First commit

---
+++ src/main/java/example/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package example.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import example.model.AppUser;
+
+public interface UserRepository extends JpaRepository<AppUser, Integer> {
+
+  boolean existsByUsername(String username);
+
+  AppUser findByUsername(String username);
+
+  @Transactional
+  void deleteByUsername(String username);
+
+}
